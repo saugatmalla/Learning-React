2173e166099c1571072bd96ec835df177b80edf8
Saugat Malla <saugat@unoapp.com>
2020-08-02 17:48:06 -0400

    Updates

---
+++ precache-manifest.feadee52714c198bdd123b4467bec2b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a689018299fb5e36d3021276e9fef95",
+    "url": "/Learning-React/index.html"
+  },
+  {
+    "revision": "7fbae4d62aa5ac46a578",
+    "url": "/Learning-React/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "5fd4582ea3a308c9309d",
+    "url": "/Learning-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7fbae4d62aa5ac46a578",
+    "url": "/Learning-React/static/js/2.c85b3184.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Learning-React/static/js/2.c85b3184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd4582ea3a308c9309d",
+    "url": "/Learning-React/static/js/main.2f80c18d.chunk.js"
+  },
+  {
+    "revision": "b142a705f62bc8106e1a",
+    "url": "/Learning-React/static/js/runtime-main.d313fb4e.js"
+  }
+]);
